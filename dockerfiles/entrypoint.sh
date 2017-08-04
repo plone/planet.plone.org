111fc740daa0514ff96937acd791b83713ce166d
sven <sven@so36.net>
2017-08-04 16:14:06 +0200

    Adjust entrypoint

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run planet on startup
-python2 /srv/venus/source /srv/venus/planet.plone.org/planet.ini
+python2 /srv/venus/source/planet.py /srv/venus/planet.plone.org/planet.ini
 
 # Start honcho
 cd /srv && honcho start
